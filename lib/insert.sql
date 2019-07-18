2bfc83dafbb752f102642c788cb4b1f86057e3a5
Niusha H <niusha.hadziomerovic@gmail.com>
2019-07-18 03:44:40 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 32, 'M', 'brown', 'hyper', '1');
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 32, 'M', 'brown', 'hyper', '1');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 32, 'M', 'brown', 'hyper', '1');
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 32, 'M', 'brown', 'hyper', '1');
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 32, 'M', 'brown', 'hyper', '1');
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 32, 'M', 'brown', 'hyper', '1');
